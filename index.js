c37089ea1c29711e38826e85fb1cc0ae2df0b57c
softwareInkhub <software.inkhub@gmail.com>
2025-04-16 23:24:00 +0530

    updaets

--- index.js
+++ index.js
@@ -2235,7 +2235,7 @@ app.post('/pinterest/token', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
   console.log(`Main API documentation available at http://localhost:${PORT}/api-docs`);
