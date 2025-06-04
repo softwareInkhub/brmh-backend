78a25e2a75db3d661da9d0337f60c057575c4cc3
softwareInkhub <software.inkhub@gmail.com>
2025-06-04 12:36:30 +0530

    updates

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ console.log('AWS Configuration Check:', {
 });
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 // File storage configuration
 const __filename = fileURLToPath(import.meta.url);
